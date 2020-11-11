3395476fcd1b6c45505554d5f674dfd9358c6bd7
Ramon Wijnands <ramon@ruvu.nl>
2020-11-11 13:23:16 +0100

    fix: NMT init sequence

--- packman_hardware/src/pacman_interface.cpp
+++ packman_hardware/src/pacman_interface.cpp
@@ -29,16 +29,18 @@ PacmanInterface::PacmanInterface()
       can_interface_.createMsgListener(can::MsgHeader(RxPDO1::ID), std::bind(&PacmanInterface::plcStateCb, this, _1));
   state_listener_ = can_interface_.createStateListener(std::bind(&PacmanInterface::CANStateCb, this, _1));
   heartbeat_listener_ = can_interface_.createMsgListener(can::MsgHeader(NMTstate::ID), [this](const can::Frame& frame) {
-    ROS_DEBUG_STREAM_NAMED(name, "Heartbeat: " << frame);
+    // ROS_DEBUG_STREAM_NAMED(name, "Heartbeat: " << frame);
     NMTstate::Frame state(frame);
     nmt_state_.store(state.data);
-    ROS_INFO_STREAM_NAMED(name, "Heartbeat: " << state);
+    ROS_DEBUG_STREAM_NAMED(name, "Heartbeat: " << state);
   });
 }
 
 PacmanInterface::~PacmanInterface()
 {
+  ROS_INFO_NAMED(name, "Sending Stop");
   can_interface_.send(NMTcommand::Frame(PLC_NODE_ID, NMTcommand::Stop));
+  ros::Duration(0.2).sleep();
   puts("Shutting down the CAN device");
   can_interface_.shutdown();
   puts("Successfully shut down the CAN device");
@@ -66,8 +68,9 @@ void PacmanInterface::init()
         ROS_INFO_NAMED(name, "PLC operational, init finished");
         return;
       case NMTstate::Stopped:
-        ROS_ERROR_NAMED(name, "PLC stopped, init failed");
-        return;
+        ROS_INFO_NAMED(name, "Sending Start");
+        can_interface_.send(NMTcommand::Frame(PLC_NODE_ID, NMTcommand::Start));
+        break;
       default:
         std::stringstream ss;
         ss << "Unknown PLC state: " << state;
